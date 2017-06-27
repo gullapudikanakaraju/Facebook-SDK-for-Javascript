deafb457ac1e68b3b0cd6acbf26fe4dbdd18936c
nalin16 <nalin.mahajan16@gmail.com>
2017-06-27 17:21:27 +0530

    added the web app

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/test',function(req,res){
+	res.sendFile(__dirname+'/public/index.html');
+});
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
